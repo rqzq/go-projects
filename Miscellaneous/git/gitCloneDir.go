af55267295855c5dfccfbdec9ce67ac60a1dfe3e
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-09-28 15:49:41 +0530

    Clone in Golang

--- Miscellaneous/git/gitCloneDir.go
+++ Miscellaneous/git/gitCloneDir.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main(){
+	// Get Current Working Directory
 	mydir, err1:= os.Getwd()
 	if err1 != nil {
 		fmt.Println(err1)
 	}
-	fmt.Println(mydir)
+	// Cloning Github Repo into Current Working Directory
 	_,err:=git.PlainClone(mydir,false,&git.CloneOptions{URL: "https://github.com/RealOrko/concourse-fetch.git",Progress: os.Stdout})
 	if err != nil {
 		fmt.Println(err)
