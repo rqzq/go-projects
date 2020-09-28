ca5e854b57f32f64d5d99554b098b43a2c56c870
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-09-28 15:46:53 +0530

    Git Clone Repository

--- Miscellaneous/git/gitCloneDir.go
+++ Miscellaneous/git/gitCloneDir.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"gopkg.in/src-d/go-git.v4"
+	"os"
+)
+
+func main(){
+	mydir, err1:= os.Getwd()
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	fmt.Println(mydir)
+	_,err:=git.PlainClone(mydir,false,&git.CloneOptions{URL: "https://github.com/RealOrko/concourse-fetch.git",Progress: os.Stdout})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
