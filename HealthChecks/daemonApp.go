62edcae1de463b35473890ac9c974a1f9913ebf7
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-09-30 18:03:08 +0530

    Added extra params

--- HealthChecks/daemonApp.go
+++ HealthChecks/daemonApp.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main(){
-	resp, err:= http.Get("https://pstexport-daemon-app-dev-2.apps.prod.smarsh.cloud/actuator/health")
+	appName:= os.Getenv("app")
+	environment:= os.Getenv("env")
+	tenant:= os.Getenv("tenant")
+	resp, err:= http.Get("https://"+ tenant + appName +"-app-" + environment +".apps.prod.smarsh.cloud/actuator/health")
 	if err != nil{
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	// Print the Status Code
 	if resp.StatusCode == 200 {
-		fmt.Println("Application is working!")
+		fmt.Println("Application",appName, "is working on",environment,"environment!")
 	} else {
 		fmt.Println("Application is Broken!")
 	}
