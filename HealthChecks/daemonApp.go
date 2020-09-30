7ed9b81d0805cb6e8d41835e3a777b688adcb17e
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-09-30 18:56:50 +0530

    Added Comment Section

--- HealthChecks/daemonApp.go
+++ HealthChecks/daemonApp.go
@@ -8,18 +8,23 @@ import (
 )
 
 func main(){
+
+	//Collect Environment Variables passed from Params Section in Concourse
 	appName:= os.Getenv("app")
 	environment:= os.Getenv("env")
 	tenant:= os.Getenv("tenant")
 	resp, err:= http.Get("https://"+ tenant + appName +"-app-" + environment +".apps.prod.smarsh.cloud/actuator/health")
+
+	//Error Handling Section
 	if err != nil{
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	// Print the Status Code
+
+	// Application Working Condition based on the Status Code
 	if resp.StatusCode == 200 {
 		fmt.Println("Application",appName, "is working on",environment,"environment!")
 	} else {
-		fmt.Println("Application is Broken!")
+		fmt.Println("Application",appName, "is Broken on",environment,"environment!")
 	}
 }
