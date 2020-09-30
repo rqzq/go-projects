20dec80b8f398035b63a94a39d130215fe0602f7
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-09-30 13:57:34 +0530

    Applying Comments on the Section of the Code

--- Miscellaneous/regexp/regex_dd.go
+++ Miscellaneous/regexp/regex_dd.go
@@ -7,13 +7,19 @@ import (
 )
 
 func main(){
+
+	// Reading the File Content
 	content, err:= ioutil.ReadFile("version.txt")
 	if err != nil{
 		fmt.Println(err)
 	}
 	version:= string(content)
+
+	// Applying the Regular Expression for matching the Characters
 	re:= regexp.MustCompile(`(?m)[\d\.]+[\.-][\d]{2,}`)
 	for _, match:= range(re.FindAllString(version,-1)){
 		fmt.Println(match)
 	}
+
+	//Executing Curl Statements for Posting Datadog Events
 }
