e8fda81cba6c840c51c5d7bf98a2c82657d7805f
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-10-17 18:40:26 +0530

    Struct with %+v which prints all the values in struct

--- structs/main.go
+++ structs/main.go
@@ -2,14 +2,30 @@ package main
 
 import "fmt"
 
+type ContactInfo struct {
+	email string
+	zipCode int
+}
+
 type Person struct {
 	firstName string
 	lastName string
+	contact ContactInfo
 }
 func main(){
 	alex:= Person{
 		firstName: "Bishwajit",
 		lastName:  "Samanta",
 	}
+	jim:= Person{
+		firstName: "Payel",
+		lastName:  "Samanta",
+		contact:   ContactInfo{
+			email:   "bishwajitsamanta1689@gmail.com",
+			zipCode: 713205,
+		},
+	}
 	fmt.Println(alex.firstName)
+	fmt.Printf("%+v",jim)
+	fmt.Println(jim.contact.email)
 }
